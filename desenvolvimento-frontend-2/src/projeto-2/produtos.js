439d08ea0a8b795dab6ac427e3e36028f3bc621d
MateusSM2000 <matt_petao@hotmail.com>
2025-05-22 13:42:40 -0300

    update

---
+++ desenvolvimento-frontend-2/src/projeto-2/produtos.js
@@ -0,0 +1,67 @@
+let produtos = [
+    {
+        'nome': 'Tênis 1',
+        'preco': 319.99,
+        'imagem': 'img.jpeg',
+    },
+    {
+        'nome': 'Tênis 2',
+        'preco': 219.99,
+        'imagem': 'img.jpeg',
+    },
+    {
+        'nome': 'Tênis 3',
+        'preco': 178.19,
+        'imagem': 'img.jpeg',
+    },
+    {
+        'nome': 'Tênis 4',
+        'preco': 278.29,
+        'imagem': 'img.jpeg',
+    },
+    {
+        'nome': 'Tênis 5',
+        'preco': 198.59,
+        'imagem': 'img.jpeg',
+    },
+    {
+        'nome': 'Tênis 5',
+        'preco': 618.19,
+        'imagem': 'img.jpeg',
+    },
+    {
+        'nome': 'Tênis 6',
+        'preco': 578.19,
+        'imagem': 'img.jpeg',
+    },
+    {
+        'nome': 'Tênis 7',
+        'preco': 999.19,
+        'imagem': 'img.jpeg',
+    },
+    {
+        'nome': 'Tênis 8',
+        'preco': 1218.99,
+        'imagem': 'img.jpeg',
+    },
+    {
+        'nome': 'Tênis 9',
+        'preco': 878.19,
+        'imagem': 'img.jpeg',
+    },
+    {
+        'nome': 'Tênis 10',
+        'preco': 318.19,
+        'imagem': 'img.jpeg',
+    },
+    {
+        'nome': 'Tênis 11',
+        'preco': 478.64,
+        'imagem': 'img.jpeg',
+    },
+    {
+        'nome': 'Tênis 12',
+        'preco': 618.48,
+        'imagem': 'img.jpeg',
+    }
+]
